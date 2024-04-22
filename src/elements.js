7da3f016206a3b98ed27434f689c8f98d9bfe596
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-04-22 23:22:24 +0200

    end of day 1

--- src/elements.js
+++ src/elements.js
@@ -12,53 +12,70 @@ class gamePlayer {
         // Define Position
         this.y = 0;
         this.x = 0;
-        // Other Properties
-        this.movementState = false;
-        this.direction = null;
+        // Movement Properties
+        this.direction = [false, false, false, false];
         this.velocity = 10;
-        this.lastDirection = null;
     };
 
     move() {
-        if (this.movementState) {
-            if (this.direction === 'up') {
-                if (this.y <= 0) {
-                    this.movementState -= false;
-                    this.y = 0;
-                    this.element.style.top = `${this.y}px`;
-                } else {
-                    this.y -= this.velocity;
-                    this.element.style.top = `${this.y}px`;
-                }
-            } else if (this.direction === 'down') {
-                if (this.y >= (this.gameAreaHeight - this.height)) {
-                    this.movementState -= false;
-                    this.y = this.gameAreaHeight - this.height;
-                    this.element.style.top = `${this.y}px`;
-                } else {
-                    this.y += this.velocity;
-                    this.element.style.top = `${this.y}px`;
-                }
-            } else if (this.direction === 'left') {
-                if (this.x <= 0) {
-                    this.movementState -= false;
-                    this.x = 0;
-                    this.element.style.left = `${this.x}px`;
-                } else {
-                    this.x -= this.velocity;
-                    this.element.style.left = `${this.x}px`;
-                }
-            } else if (this.direction === 'right') {
-                if (this.x >= (this.gameAreaWidth - this.width)) {
-                    this.movementState -= false;
-                    this.x = this.gameAreaWidth - this.width;
-                    console.log(this.gameAreaWidth, this.width);
-                    this.element.style.left = `${this.x}px`;
-                } else {
-                    this.x += this.velocity;
-                    this.element.style.left = `${this.x}px`;
-                };
+        // Moving up + game boundaries
+        if (this.direction[0]) {
+            if (this.y <= 0) {
+                this.y = 0;
+                this.element.style.top = `${this.y}px`;
+            } else {
+                this.y -= this.velocity;
+                this.element.style.top = `${this.y}px`;
+            }
+            // Moving down + game boundaries
+        }  if (this.direction[1]) {
+            if (this.y >= (this.gameAreaHeight - this.height)) {
+                this.y = this.gameAreaHeight - this.height;
+                this.element.style.top = `${this.y}px`;
+            } else {
+                this.y += this.velocity;
+                this.element.style.top = `${this.y}px`;
+            }
+            // Moving left + game boundaries
+        }  if (this.direction[2]) {
+            if (this.x <= 0) {
+                this.x = 0;
+                this.element.style.left = `${this.x}px`;
+            } else {
+                this.x -= this.velocity;
+                this.element.style.left = `${this.x}px`;
+            }
+            // Moving right + game boundaries
+        }  if (this.direction[3]) {
+            if (this.x >= (this.gameAreaWidth - this.width)) {
+                this.x = this.gameAreaWidth - this.width;
+                this.element.style.left = `${this.x}px`;
+            } else {
+                this.x += this.velocity;
+                this.element.style.left = `${this.x}px`;
             };
         };
+    };
+};
+
+class gameEnemy {
+    constructor(gameAreaElement) {
+        // Define Element
+        this.element = document.querySelector("#enemy");
+        // Define Size
+        this.height = this.element.getBoundingClientRect().height;
+        this.width = this.element.getBoundingClientRect().width;
+        // Define Boundaries
+        this.gameAreaHeight = gameAreaElement.getBoundingClientRect().height;
+        this.gameAreaWidth = gameAreaElement.getBoundingClientRect().width;
+        // Define Position
+        this.y = this.gameAreaHeight / 2;
+        this.x = this.gameAreaWidth / 2;
+        // Movement Properties
+        this.direction = [false, false, false, false];
+        this.velocity = 10;
+        this.idleVelocity = this.velocity/2;
+        this.idleRadius = 150;
+        this.ChaseRadius = 50000;
     };
 };
--- src/script.js
+++ src/script.js
@@ -1,11 +1,117 @@
 console.log("Hello World! I am alive!");
 
 let frameCounter = 0;
-const arrayDirection = [false, false, false, false];
 
 const gameAreaElement = document.querySelector("#game-area");
 const playerElement = new gamePlayer(gameAreaElement);
+const enemyElement = new gameEnemy(gameAreaElement);
 
+let idleTopCornerRight = enemyElement.x + enemyElement.idleRadius;
+let firstFlag = false;
+let idleBottomCornerRight = enemyElement.y + enemyElement.idleRadius;
+let secondFlag = false;
+let idleTopCornerLeft = enemyElement.x - enemyElement.idleRadius;
+let thirdFlag = false;
+let idleBottomCornerLeft = enemyElement.y - enemyElement.idleRadius;
+
+// Checking if player or enemy collide with each other
+function collissionCheck() {
+    if (
+        (
+            // Checks right upper corner of player agains enemy width position
+            (
+                (playerElement.x + playerElement.width) <= (enemyElement.x + enemyElement.width)
+                && (playerElement.x + playerElement.width) >= enemyElement.x
+            )
+            //  Checks left upper corner of player element against enemy width position
+            || (
+                playerElement.x >= enemyElement.x
+                && (playerElement.x <= enemyElement.x + enemyElement.width)
+            )
+        )
+        && (
+            // Checks upper left corner of player agains enemy height position
+            (
+                playerElement.y >= enemyElement.y
+                && (playerElement.y <= enemyElement.y + enemyElement.height)
+            )
+            // Checks lower left corner of player agains enemy height position
+            || (
+                (playerElement.y + playerElement.height) <= (enemyElement.y + enemyElement.height)
+                && (playerElement.y + playerElement.height) >= enemyElement.y
+            )
+        )
+    ) {
+        console.log("CRASH");
+    }
+}
+
+function chase() {
+    // randomly moves the enemy on the X axis towards the player direction
+    if (Math.floor(Math.random() * 2) === 0 && enemyElement.x !== playerElement.x) {
+        if (enemyElement.x + enemyElement.width >= playerElement.x + playerElement.width) {
+            enemyElement.x -= enemyElement.velocity;
+            if (enemyElement.x <= playerElement.x) {
+                enemyElement.x = playerElement.x;
+            }
+            enemyElement.element.style.left = `${enemyElement.x}px`;
+        } else {
+            enemyElement.x += enemyElement.velocity;
+            enemyElement.element.style.left = `${enemyElement.x}px`;
+        }
+        // randomly moves the enemy on the Y axis towards the player direction
+    } else if (Math.floor(Math.random() * 2) === 1 && enemyElement.y !== playerElement.y) {
+        if (enemyElement.y + enemyElement.height >= playerElement.y + playerElement.height) {
+            enemyElement.y -= enemyElement.velocity;
+            if (enemyElement.y <= playerElement.y) {
+                enemyElement.y = playerElement.y;
+            }
+            enemyElement.element.style.top = `${enemyElement.y}px`;
+        } else {
+            enemyElement.y += enemyElement.velocity;
+            enemyElement.element.style.top = `${enemyElement.y}px`;
+        }
+    };
+};
+
+function idleMovement() {
+    if (!firstFlag) {
+        enemyElement.x += enemyElement.idleVelocity;
+        if (enemyElement.x >= idleTopCornerRight) {
+            enemyElement.x = idleTopCornerRight;
+            firstFlag = true;
+        };
+        enemyElement.element.style.left = `${enemyElement.x}px`;
+    }
+    if (!secondFlag & firstFlag) {
+        enemyElement.y += enemyElement.idleVelocity;
+        if (enemyElement.y >= idleBottomCornerRight) {
+            enemyElement.y = idleBottomCornerRight;
+            secondFlag = true;
+        };
+        enemyElement.element.style.top = `${enemyElement.y}px`;
+    };
+    if (!thirdFlag & secondFlag & firstFlag) {
+        enemyElement.x -= enemyElement.idleVelocity;
+        if (enemyElement.x <= idleTopCornerLeft) {
+            enemyElement.x = idleTopCornerLeft;
+            thirdFlag = true;
+        };
+        enemyElement.element.style.left = `${enemyElement.x}px`;
+    };
+    if (thirdFlag & secondFlag & firstFlag) {
+        enemyElement.y -= enemyElement.idleVelocity;
+        if (enemyElement.y <= idleBottomCornerLeft) {
+            enemyElement.y = idleBottomCornerLeft;
+            firstFlag = false
+            secondFlag = false
+            thirdFlag = false
+        };
+        enemyElement.element.style.top = `${enemyElement.y}px`;
+    };
+};
+
+let distance = 0;
 
 function gameLoop() {
     // Add here all the movement that is going to be trigered on each frame
@@ -13,7 +119,15 @@ function gameLoop() {
     // enemiesArray.forEach((enemy) => {
     //   enemy.move();
     // });
-    // collissionCheck();
+    distance = (((playerElement.x + playerElement.width / 2) - (enemyElement.x + enemyElement.width / 2)) ** 2 +
+        ((playerElement.y + playerElement.height / 2) - (enemyElement.y + enemyElement.height / 2)) ** 2) ** 1 / 2;
+    console.log(distance);
+    if (distance <= enemyElement.ChaseRadius){
+        chase();
+    } else{
+        idleMovement();
+    }
+    collissionCheck();
     playerElement.move();
     window.requestAnimationFrame(gameLoop);
 }
@@ -22,46 +136,25 @@ window.requestAnimationFrame(gameLoop);
 
 document.addEventListener("keydown", (keyEvent) => {
     if (keyEvent.key === "w") {
-        arrayDirection[0] = true;
-        playerElement.direction = 'up';
-        playerElement.movementState = true;
+        playerElement.direction[0] = true;
     } else if (keyEvent.key === "s") {
-        arrayDirection[1] = true;
-        playerElement.direction = 'down';
-        playerElement.movementState = true;
+        playerElement.direction[1] = true;
     } else if (keyEvent.key === "a") {
-        arrayDirection[2] = true;
-        playerElement.direction = 'left';
-        playerElement.movementState = true;
+        playerElement.direction[2] = true;
     } else if (keyEvent.key === "d") {
-        arrayDirection[3] = true;
-        playerElement.direction = 'right';
-        playerElement.movementState = true;
+        playerElement.direction[3] = true;
     };
 });
 
 
 document.addEventListener("keyup", (keyEvent) => {
     if (keyEvent.key === "w") {
-        arrayDirection[0] = false;
+        playerElement.direction[0] = false;
     } else if (keyEvent.key === "s") {
-        arrayDirection[1] = false;
+        playerElement.direction[1] = false;
     } else if (keyEvent.key === "a") {
-        arrayDirection[2] = false;
+        playerElement.direction[2] = false;
     } else if (keyEvent.key === "d") {
-        arrayDirection[3] = false;
-    };
-
-    if (arrayDirection.every(element => element === false)) {
-        playerElement.movementState = false;
-        console.log(playerElement.x, playerElement.y)
-    } else if (arrayDirection.indexOf(true) === 0){
-        playerElement.direction = 'up';
-    } else if (arrayDirection.indexOf(true) === 1){
-        playerElement.direction = 'down';
-    } else if (arrayDirection.indexOf(true) === 2){
-        playerElement.direction = 'left';
-    } else if (arrayDirection.indexOf(true) === 3){
-        playerElement.direction = 'right';
+        playerElement.direction[3] = false;
     };
 });
