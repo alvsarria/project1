c1dbd5086d9c1fa9e78f3417284ab25361a4fb79
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-04-23 21:44:53 +0200

    day 2

--- src/elements.js
+++ src/elements.js
@@ -15,6 +15,7 @@ class gamePlayer {
         // Movement Properties
         this.direction = [false, false, false, false];
         this.velocity = 10;
+        this.distance = null;
     };
 
     move() {
@@ -28,7 +29,7 @@ class gamePlayer {
                 this.element.style.top = `${this.y}px`;
             }
             // Moving down + game boundaries
-        }  if (this.direction[1]) {
+        } if (this.direction[1]) {
             if (this.y >= (this.gameAreaHeight - this.height)) {
                 this.y = this.gameAreaHeight - this.height;
                 this.element.style.top = `${this.y}px`;
@@ -37,7 +38,7 @@ class gamePlayer {
                 this.element.style.top = `${this.y}px`;
             }
             // Moving left + game boundaries
-        }  if (this.direction[2]) {
+        } if (this.direction[2]) {
             if (this.x <= 0) {
                 this.x = 0;
                 this.element.style.left = `${this.x}px`;
@@ -46,7 +47,7 @@ class gamePlayer {
                 this.element.style.left = `${this.x}px`;
             }
             // Moving right + game boundaries
-        }  if (this.direction[3]) {
+        } if (this.direction[3]) {
             if (this.x >= (this.gameAreaWidth - this.width)) {
                 this.x = this.gameAreaWidth - this.width;
                 this.element.style.left = `${this.x}px`;
@@ -56,26 +57,339 @@ class gamePlayer {
             };
         };
     };
+
+    playerCollission(collisionObject) {
+        if (
+            (
+                // Checks right upper corner of player agains enemy width position
+                (
+                    this.x >= collisionObject.x
+                    && this.x <= collisionObject.x + collisionObject.width
+                )
+                //  Checks left upper corner of player element against enemy width position
+                || (
+                    this.x + this.width <= collisionObject.width + collisionObject.x
+                    && this.x + this.width >= collisionObject.x
+                )
+            )
+            && (
+                // Checks upper left corner of player agains enemy height position
+                (
+                    this.y >= collisionObject.y
+                    && this.y <= collisionObject.y + collisionObject.height
+                )
+                // Checks lower left corner of player agains enemy height position
+                || (
+                    this.y + this.height <= collisionObject.y + collisionObject.height
+                    && this.y + this.height >= collisionObject.y
+                )
+            )
+        ) {
+            let diffX = (this.x + this.width / 2) - (collisionObject.x + collisionObject.width / 2);
+            let diffY = (collisionObject.y + collisionObject.height / 2) - (this.y + this.height / 2);
+            if (diffX < 0) {
+                if (diffY < 0) {
+                    if (Math.abs(diffY) > Math.abs(diffX)) {
+                        this.y = collisionObject.y + collisionObject.height;
+                        this.element.style.top = `${this.y}px`;
+                        console.log("CRASH FROM TOP");
+                    } else {
+                        this.x = collisionObject.x - this.width;
+                        this.element.style.left = `${this.x}px`;
+                        console.log("CRASH FROM RIGHT");
+                    };
+                } else {
+                    if (Math.abs(diffY) > Math.abs(diffX)) {
+                        this.y = collisionObject.y - this.height;
+                        this.element.style.top = `${this.y}px`;
+                        console.log("CRASH FROM BOTTOM");
+                    } else {
+                        this.x = collisionObject.x - this.width;
+                        this.element.style.left = `${this.x}px`;
+                        console.log("CRASH FROM RIGHT");
+                    };
+                };
+            } else {
+                if (diffY < 0) {
+                    if (Math.abs(diffY) > Math.abs(diffX)) {
+                        this.y = collisionObject.y + collisionObject.height;
+                        this.element.style.top = `${this.y}px`;
+                        console.log("CRASH FROM TOP");
+                    } else {
+                        this.x = collisionObject.x + collisionObject.width;
+                        this.element.style.left = `${this.x}px`;
+                        console.log("CRASH FROM LEFT");
+                    };
+                } else {
+                    if (Math.abs(diffY) > Math.abs(diffX)) {
+                        this.y = collisionObject.y - this.height;
+                        this.element.style.top = `${this.y}px`;
+                        console.log("CRASH FROM BOTTOM");
+                    } else {
+                        this.x = collisionObject.x + collisionObject.width;
+                        this.element.style.left = `${this.x}px`;
+                        console.log("CRASH FROM LEFT");
+                    };
+                };
+                return true;
+            };
+        } else {
+            return false;
+        };
+    };
 };
 
 class gameEnemy {
     constructor(gameAreaElement) {
-        // Define Element
-        this.element = document.querySelector("#enemy");
-        // Define Size
-        this.height = this.element.getBoundingClientRect().height;
-        this.width = this.element.getBoundingClientRect().width;
         // Define Boundaries
+        this.gameAreaElement = document.querySelector("#game-area");
         this.gameAreaHeight = gameAreaElement.getBoundingClientRect().height;
         this.gameAreaWidth = gameAreaElement.getBoundingClientRect().width;
         // Define Position
-        this.y = this.gameAreaHeight / 2;
-        this.x = this.gameAreaWidth / 2;
+        this.x = 0;
+        this.y = 0;
+        this.width = 0;
+        this.height = 0;
+        // Define Boundaries
+        this.gameAreaHeight = gameAreaElement.getBoundingClientRect().height;
+        this.gameAreaWidth = gameAreaElement.getBoundingClientRect().width;
         // Movement Properties
         this.direction = [false, false, false, false];
         this.velocity = 10;
-        this.idleVelocity = this.velocity/2;
+        this.ChaseRadius = 50000 * 2;
+        // Idle Movement Properties
+        this.idleVelocity = this.velocity / 2;
         this.idleRadius = 150;
-        this.ChaseRadius = 50000;
+        this.idleTopCornerRight = null;
+        this.firstFlag = false;
+        this.idleBottomCornerRight = null;
+        this.secondFlag = false;
+        this.idleTopCornerLeft = null;
+        this.thirdFlag = false;
+        this.idleBottomCornerLeft = null;
+    };
+
+    createElement(classObject) {
+        this.element = document.createElement("div");
+        this.element.classList.add(classObject);
+        // remember the bug we had in class? We have to append the element to the game area before we can get its width and height!
+        this.gameAreaElement.appendChild(this.element);
+        this.height = this.element.getBoundingClientRect().height;
+        this.width = this.element.getBoundingClientRect().width;
+        const yCoordinate = Math.floor(Math.random() * (this.gameAreaHeight - this.height))
+        const xCoordinate = Math.floor(Math.random() * (this.gameAreaWidth - this.width))
+        this.y = yCoordinate;
+        this.x = xCoordinate;
+        this.element.style.top = `${yCoordinate}px`;
+        this.element.style.left = `${xCoordinate}px`;
+        this.idleTopCornerRight = this.x + this.idleRadius;
+        this.idleBottomCornerRight = this.y + this.idleRadius;
+        this.idleTopCornerLeft = this.x - this.idleRadius;
+        this.idleBottomCornerLeft = this.y - this.idleRadius;
+    };
+
+    enemyCollission(collisionObject) {
+        if (
+            (
+                // Checks right upper corner of player agains enemy width position
+                (
+                    this.x >= collisionObject.x
+                    && this.x <= collisionObject.x + collisionObject.width
+                )
+                //  Checks left upper corner of player element against enemy width position
+                || (
+                    this.x + this.width <= collisionObject.width + collisionObject.x
+                    && this.x + this.width >= collisionObject.x
+                )
+            )
+            && (
+                // Checks upper left corner of player agains enemy height position
+                (
+                    this.y >= collisionObject.y
+                    && this.y <= collisionObject.y + collisionObject.height
+                )
+                // Checks lower left corner of player agains enemy height position
+                || (
+                    this.y + this.height <= collisionObject.y + collisionObject.height
+                    && this.y + this.height >= collisionObject.y
+                )
+            )
+        ) {
+            let diffX = (this.x + this.width / 2) - (collisionObject.x + collisionObject.width / 2);
+            let diffY = (collisionObject.y + collisionObject.height / 2) - (this.y + this.height / 2);
+            if (diffX < 0) {
+                if (diffY < 0) {
+                    if (Math.abs(diffY) > Math.abs(diffX)) {
+                        this.y = collisionObject.y + collisionObject.height;
+                        this.element.style.top = `${this.y}px`;
+                        // console.log("CRASH FROM BOTTOM");
+                    } else {
+                        this.x = collisionObject.x - this.width;
+                        this.element.style.left = `${this.x}px`;
+                        // console.log("CRASH FROM RIGHT");
+                    };
+                } else {
+                    if (Math.abs(diffY) > Math.abs(diffX)) {
+                        this.y = collisionObject.y - this.height;
+                        this.element.style.top = `${this.y}px`;
+                        // console.log("CRASH FROM TOP");
+                    } else {
+                        this.x = collisionObject.x - this.width;
+                        this.element.style.left = `${this.x}px`;
+                        // console.log("CRASH FROM RIGHT");
+                    };
+                };
+            } else {
+                if (diffY < 0) {
+                    if (Math.abs(diffY) > Math.abs(diffX)) {
+                        this.y = collisionObject.y + collisionObject.height;
+                        this.element.style.top = `${this.y}px`;
+                        // console.log("CRASH FROM BOTTOM");
+                    } else {
+                        this.x = collisionObject.x + collisionObject.width;
+                        this.element.style.left = `${this.x}px`;
+                        // console.log("CRASH FROM LEFT");
+                    };
+                } else {
+                    if (Math.abs(diffY) > Math.abs(diffX)) {
+                        this.y = collisionObject.y - this.height;
+                        this.element.style.top = `${this.y}px`;
+                        // console.log("CRASH FROM TOP");
+                    } else {
+                        this.x = collisionObject.x + collisionObject.width;
+                        this.element.style.left = `${this.x}px`;
+                        // console.log("CRASH FROM LEFT");
+                    };
+                };
+                return true;
+            };
+        } else {
+            return false;
+        };
+    };
+
+    idleState(playerObject) {
+        this.distance = (((playerObject.x + playerObject.width / 2) - (this.x + this.width / 2)) ** 2 +
+            ((playerObject.y + playerObject.height / 2) - (this.y + this.height / 2)) ** 2) ** 1 / 2;
+        if (this.distance <= this.ChaseRadius) {
+            return true;
+        } else {
+            return false;
+        };
+    };
+
+    idleMovement() {
+        // Starts idle movement towards right 
+        if (!this.firstFlag) {
+            this.x += this.idleVelocity;
+            if (this.x + this.width >= this.gameAreaWidth) {
+                this.firstFlag = true;
+                this.x = this.gameAreaWidth - this.width;
+            } else if (this.x >= this.idleTopCornerRight) {
+                this.firstFlag = true;
+            };
+            this.element.style.left = `${this.x}px`;
+        };
+        // Ctarts idle movement towards down
+        if (!this.secondFlag & this.firstFlag) {
+            this.y += this.idleVelocity;
+            if (this.y + this.height >= this.gameAreaHeight) {
+                this.secondFlag = true;
+                this.y = this.gameAreaHeight - this.height;
+            } else if (this.y >= this.idleBottomCornerRight) {
+                this.secondFlag = true;
+            };
+            this.element.style.top = `${this.y}px`;
+        };
+        // Continues idle movement towards right
+        if (!this.thirdFlag & this.secondFlag & this.firstFlag) {
+            this.x -= this.idleVelocity;
+            if (this.x <= 0) {
+                this.thirdFlag = true;
+                this.x = 0
+            } else if (this.x <= this.idleTopCornerLeft) {
+                this.thirdFlag = true;
+            };
+            this.element.style.left = `${this.x}px`;
+        };
+        // Continues idle movement towards up and resets movement
+        if (this.thirdFlag & this.secondFlag & this.firstFlag) {
+            this.y -= this.idleVelocity;
+            if (this.y <= 0) {
+                this.firstFlag = false;
+                this.secondFlag = false;
+                this.thirdFlag = false;
+                this.y = 0;
+            } else if (this.y <= this.idleBottomCornerLeft) {
+                this.firstFlag = false;
+                this.secondFlag = false;
+                this.thirdFlag = false;
+            };
+            this.element.style.top = `${this.y}px`;
+        };
+    };
+
+    chase(playerObject) {
+        // randomly moves the enemy on the X axis towards the player direction
+        if (Math.floor(Math.random() * 2) === 0 && this.x !== playerObject.x) {
+            if (this.x > playerObject.width + playerObject.x) {
+                this.x -= this.velocity;
+                if (this.x < playerObject.width + playerObject.x) {
+                    this.x = playerObject.width + playerObject.x;
+                };
+                this.element.style.left = `${this.x}px`;
+            } else if (this.x + this.width < playerObject.x) {
+                this.x += this.velocity;
+                if (this.x + this.width > playerObject.x) {
+                    this.x = playerObject.x - this.width;
+                };
+                this.element.style.left = `${this.x}px`;
+            };
+            // randomly moves the enemy on the Y axis towards the player direction
+        } else if (Math.floor(Math.random() * 2) === 1 && this.y !== playerObject.y) {
+            if (this.y > playerObject.height + playerObject.y) {
+                this.y -= this.velocity;
+                if (this.y < playerObject.height + playerObject.y) {
+                    this.y = playerObject.height + playerObject.y;
+                };
+                this.element.style.top = `${this.y}px`;
+            } else if (this.y + this.height < playerObject.y) {
+                this.y += this.velocity;
+                if (this.y + this.height > playerObject.y) {
+                    this.y = playerObject.y - this.height;
+                };
+                this.element.style.top = `${this.y}px`;
+            };
+        };
+    };
+};
+
+class fieldObject {
+    constructor(classObject) {
+        this.classObject = classObject;
+        // Define Boundaries
+        this.gameAreaElement = document.querySelector("#game-area");
+        this.gameAreaHeight = gameAreaElement.getBoundingClientRect().height;
+        this.gameAreaWidth = gameAreaElement.getBoundingClientRect().width;
+        // Define Position
+        this.x = 0;
+        this.y = 0;
+        this.width = 0;
+        this.height = 0;
+        //this.createElement();
+    };
+
+    createElement(xCoordinate, yCoordinate) {
+        this.element = document.createElement("div");
+        this.element.classList.add(this.classObject);
+        // remember the bug we had in class? We have to append the element to the game area before we can get its width and height!
+        this.gameAreaElement.appendChild(this.element);
+
+        this.element.style.top = `${yCoordinate}px`;
+        this.element.style.left = `${xCoordinate}px`;
+        this.y = yCoordinate;
+        this.x = xCoordinate;
+        this.height = this.element.getBoundingClientRect().height;
+        this.width = this.element.getBoundingClientRect().width;
     };
 };
--- src/script.js
+++ src/script.js
@@ -5,130 +5,33 @@ let frameCounter = 0;
 const gameAreaElement = document.querySelector("#game-area");
 const playerElement = new gamePlayer(gameAreaElement);
 const enemyElement = new gameEnemy(gameAreaElement);
+enemyElement.createElement("enemy")
 
-let idleTopCornerRight = enemyElement.x + enemyElement.idleRadius;
-let firstFlag = false;
-let idleBottomCornerRight = enemyElement.y + enemyElement.idleRadius;
-let secondFlag = false;
-let idleTopCornerLeft = enemyElement.x - enemyElement.idleRadius;
-let thirdFlag = false;
-let idleBottomCornerLeft = enemyElement.y - enemyElement.idleRadius;
 
-// Checking if player or enemy collide with each other
-function collissionCheck() {
-    if (
-        (
-            // Checks right upper corner of player agains enemy width position
-            (
-                (playerElement.x + playerElement.width) <= (enemyElement.x + enemyElement.width)
-                && (playerElement.x + playerElement.width) >= enemyElement.x
-            )
-            //  Checks left upper corner of player element against enemy width position
-            || (
-                playerElement.x >= enemyElement.x
-                && (playerElement.x <= enemyElement.x + enemyElement.width)
-            )
-        )
-        && (
-            // Checks upper left corner of player agains enemy height position
-            (
-                playerElement.y >= enemyElement.y
-                && (playerElement.y <= enemyElement.y + enemyElement.height)
-            )
-            // Checks lower left corner of player agains enemy height position
-            || (
-                (playerElement.y + playerElement.height) <= (enemyElement.y + enemyElement.height)
-                && (playerElement.y + playerElement.height) >= enemyElement.y
-            )
-        )
-    ) {
-        console.log("CRASH");
-    }
-}
+const fieldObject1 = new fieldObject("lake");
+fieldObject1.createElement(328, 500);
+const fieldObject2 = new fieldObject("rock");
+fieldObject2.createElement(1200, 200);
+const fieldObject3 = new fieldObject("tree");
+fieldObject3.createElement(1400, 700);
+const fieldObject4 = new fieldObject("tree");
+fieldObject4.createElement(1400, 550);
 
-function chase() {
-    // randomly moves the enemy on the X axis towards the player direction
-    if (Math.floor(Math.random() * 2) === 0 && enemyElement.x !== playerElement.x) {
-        if (enemyElement.x + enemyElement.width >= playerElement.x + playerElement.width) {
-            enemyElement.x -= enemyElement.velocity;
-            if (enemyElement.x <= playerElement.x) {
-                enemyElement.x = playerElement.x;
-            }
-            enemyElement.element.style.left = `${enemyElement.x}px`;
-        } else {
-            enemyElement.x += enemyElement.velocity;
-            enemyElement.element.style.left = `${enemyElement.x}px`;
-        }
-        // randomly moves the enemy on the Y axis towards the player direction
-    } else if (Math.floor(Math.random() * 2) === 1 && enemyElement.y !== playerElement.y) {
-        if (enemyElement.y + enemyElement.height >= playerElement.y + playerElement.height) {
-            enemyElement.y -= enemyElement.velocity;
-            if (enemyElement.y <= playerElement.y) {
-                enemyElement.y = playerElement.y;
-            }
-            enemyElement.element.style.top = `${enemyElement.y}px`;
-        } else {
-            enemyElement.y += enemyElement.velocity;
-            enemyElement.element.style.top = `${enemyElement.y}px`;
-        }
-    };
-};
-
-function idleMovement() {
-    if (!firstFlag) {
-        enemyElement.x += enemyElement.idleVelocity;
-        if (enemyElement.x >= idleTopCornerRight) {
-            enemyElement.x = idleTopCornerRight;
-            firstFlag = true;
-        };
-        enemyElement.element.style.left = `${enemyElement.x}px`;
-    }
-    if (!secondFlag & firstFlag) {
-        enemyElement.y += enemyElement.idleVelocity;
-        if (enemyElement.y >= idleBottomCornerRight) {
-            enemyElement.y = idleBottomCornerRight;
-            secondFlag = true;
-        };
-        enemyElement.element.style.top = `${enemyElement.y}px`;
-    };
-    if (!thirdFlag & secondFlag & firstFlag) {
-        enemyElement.x -= enemyElement.idleVelocity;
-        if (enemyElement.x <= idleTopCornerLeft) {
-            enemyElement.x = idleTopCornerLeft;
-            thirdFlag = true;
-        };
-        enemyElement.element.style.left = `${enemyElement.x}px`;
-    };
-    if (thirdFlag & secondFlag & firstFlag) {
-        enemyElement.y -= enemyElement.idleVelocity;
-        if (enemyElement.y <= idleBottomCornerLeft) {
-            enemyElement.y = idleBottomCornerLeft;
-            firstFlag = false
-            secondFlag = false
-            thirdFlag = false
-        };
-        enemyElement.element.style.top = `${enemyElement.y}px`;
-    };
-};
-
-let distance = 0;
+arrayFieldObjects = [fieldObject1, fieldObject2, fieldObject3, fieldObject4]
 
 function gameLoop() {
     // Add here all the movement that is going to be trigered on each frame
-    frameCounter++;
-    // enemiesArray.forEach((enemy) => {
-    //   enemy.move();
-    // });
-    distance = (((playerElement.x + playerElement.width / 2) - (enemyElement.x + enemyElement.width / 2)) ** 2 +
-        ((playerElement.y + playerElement.height / 2) - (enemyElement.y + enemyElement.height / 2)) ** 2) ** 1 / 2;
-    console.log(distance);
-    if (distance <= enemyElement.ChaseRadius){
-        chase();
-    } else{
-        idleMovement();
-    }
-    collissionCheck();
     playerElement.move();
+    frameCounter++;
+    if (enemyElement.idleState(playerElement)) {
+        enemyElement.chase(playerElement)
+    } else {
+        enemyElement.idleMovement();
+    }
+    enemyElement.chase(playerElement);
+    arrayFieldObjects.forEach(element => playerElement.playerCollission(element));
+    arrayFieldObjects.forEach(element => enemyElement.enemyCollission(element));
+    playerElement.playerCollission(enemyElement);
     window.requestAnimationFrame(gameLoop);
 }
 
